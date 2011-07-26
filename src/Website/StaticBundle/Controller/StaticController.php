bd422325125981ae57e7089c81000c5c92327810
Alexander Kondratenko <a.kondratenko@pegast.ru>
2011-07-26 14:10:42 +0400

    Style fixes of currency ratesrrency

--- src/Website/StaticBundle/Controller/StaticController.php
+++ src/Website/StaticBundle/Controller/StaticController.php
@@ -43,7 +43,7 @@ class StaticController extends Controller
     $rate = null;
     $sourceURL = 'http://pegast.ru/samo5/currency';
     $expirationPeriod = 60 * 60;
-    $cacheFile = $this->get('kernel')->getCacheDir() . '/currency';
+    $cacheFile = $this->get('kernel')->getCacheDir() . '/external/currency.cache';
 
     if (
       !is_file($cacheFile) ||
