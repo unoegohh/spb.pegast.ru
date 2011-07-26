d2009846e18fd78cbdfb170cdee3ad0e1180a2eb
Alexander Kondratenko <a.kondratenko@pegast.ru>
2011-07-26 13:50:29 +0400

    Added external content with currency rates

--- src/Website/StaticBundle/Controller/StaticController.php
+++ src/Website/StaticBundle/Controller/StaticController.php
@@ -4,6 +4,7 @@ namespace Website\StaticBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\Response;
 use Knp\Bundle\MenuBundle\MenuItem;
 
 
@@ -36,5 +37,37 @@ class StaticController extends Controller
     $menu->addChild('Полезная информация', $this->generateUrl('info'));
     $menu->addChild('Погода на курортах', $this->generateUrl('weather'));
     return $this->render('WebsiteStaticBundle:Static:menu.html.twig', array('menu' => $menu->toArray()));
+  }
+
+  public function currencyRateAction() {
+    $rate = null;
+    $sourceURL = 'http://pegast.ru/samo5/currency';
+    $expirationPeriod = 60 * 60;
+    $cacheFile = $this->get('kernel')->getCacheDir() . '/currency';
+
+    if (
+      !is_file($cacheFile) ||
+      (
+       time() - filectime($cacheFile) >= $expirationPeriod &&
+       $rate = @file_get_contents($sourceURL)
+      )
+    ) {
+      // Дублирование кода - плохо, но иначе нельзя
+      // Получаем курсы валют в случае отсутствия файла кэша
+      if (!$rate) {
+        $rate = @file_get_contents($sourceURL);
+      }
+      // Отрезаем нативный <link> (чужие стили не нужны) и <script>
+      $rate = preg_replace(array('/<link[^<>]+>/im', '!<script.*?>.*?</script>!im'), '', $rate);
+      // Сохраняем $rate в файле кэша
+      @file_put_contents($cacheFile, $rate);
+    }
+    // Если файл кэша существует и его размер больше 0, то читаем его содежимое
+    elseif (is_file($cacheFile) && filesize($cacheFile) > 0) {
+      $rate = @file_get_contents($cacheFile);
+    }
+
+    $response = new Response(iconv('CP1251', 'UTF-8', $rate));
+    return $response;
   }
 }
