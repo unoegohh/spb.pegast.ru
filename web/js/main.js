8447082798439445c96a8e094409133142a58d12
Alexander Kondratenko <a.kondratenko@pegast.ru>
2011-12-09 14:06:24 +0400

    Fixed menu in StaticController & added JS for changing height of content divs

---
+++ web/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: a.kondratenko
+ * Date: 09.12.11
+ * Time: 12:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function(){
+    var leftHeight = $('div#content-left').height(),
+        mainHeight = $('div#content-main').height(),
+        rightHeight = $('div#content-right').height();
+
+    var maxHeight = Math.max(leftHeight, mainHeight, rightHeight);
+
+    $('div#content-left,div#content-main,div#content-right').height(maxHeight);
+});
